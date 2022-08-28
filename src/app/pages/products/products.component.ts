01a9b7034d6347a6d5ccc22bd819c638635609ce
nadub10 <nadia.batyk@gmail.com>
2022-08-28 13:19:56 -0300

    product card

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/models/product';
+import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: Array<Product> = [
+    {
+      name: 'Pizza',
+      price: 1800,
+      image:
+        'https://cdn3.iconfinder.com/data/icons/street-food-and-food-trucker-1/64/pizza-fast-food-bake-bread-256.png',
+    },
+  ];
+  constructor(private productsService: ProductsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
